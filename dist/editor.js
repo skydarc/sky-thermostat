ade253cf660e2da7dc7ca998bc9530583a03a8a3
skydarc <65732104+skydarc@users.noreply.github.com>
2025-02-09 14:26:20 +0100

    Update editor.js

--- dist/editor.js
+++ dist/editor.js
@@ -38,5 +38,42 @@ class skyThermostatEditor extends HTMLElement {
         return this.config;
     }
 }
-
 customElements.define('sky-thermostat-editor', skyThermostatEditor);
+
+class skyDhwControlerEditor extends HTMLElement {
+    
+    async setConfig(config) {
+        this._config = { ...config, entities: { ...(config.entities || {}) } };
+        
+        await libEditor.loadTranslations(this);
+    
+        if (!this.shadowRoot) {
+            
+            this.attachShadow({ mode: 'open' });
+            
+            this.shadowRoot.innerHTML = `
+                <div id="content" class="content">test</div>
+            `;
+            
+            const container = this.shadowRoot;
+            const style = document.createElement('style');
+            style.textContent = css();
+            container.appendChild(style);
+            
+        }
+        libEditor.renderDhwContent(this);
+    }
+  
+    set hass(hass) {
+        this._hass = hass;
+    }
+      
+    get hass() {
+        return this._hass;
+    }
+      
+    get value() {
+        return this.config;
+    }
+}
+customElements.define('sky-dhw-controler-editor', skyDhwControlerEditor);
